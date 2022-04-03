2831366077bd14ec5a40f81c3a71423a9f3681d5
Egor Tarasov <jorres.tarasov@gmail.com>
2022-04-04 02:17:07 +0300

    feat: disable hack

--- painless/painless-src/working/DivideAndConquer.cpp
+++ painless/painless-src/working/DivideAndConquer.cpp
@@ -255,22 +255,22 @@ DivideAndConquer::DivideAndConquer()
 
    workQueue = new SimpleWorkingQueue();
 
-   const char* filename = Parameters::getFilename();
-   std::string s(filename);
-   std::string cube_filename = s.substr(0, s.find('.')) + std::string(".txt");
-   std::string input;
+   // const char* filename = Parameters::getFilename();
+   // std::string s(filename);
+   // std::string cube_filename = s.substr(0, s.find('.')) + std::string(".txt");
+   // std::string input;
 
-   std::ifstream cubes_file(cube_filename.c_str());
+   // std::ifstream cubes_file(cube_filename.c_str());
 
-   while (std::getline(cubes_file, input)) {
-       std::stringstream iss(input);
-       int x;
-       vector<int> cube;
-       while (iss >> x) {
-           cube.push_back(x);
-       }
-       workQueue->pushJob(cube);
-   }
+   // while (std::getline(cubes_file, input)) {
+   //     std::stringstream iss(input);
+   //     int x;
+   //     vector<int> cube;
+   //     while (iss >> x) {
+   //         cube.push_back(x);
+   //     }
+   //     workQueue->pushJob(cube);
+   // }
 
    waitJob = true;
 
